acd434f85cfd0f5bd4fa2d8f342d3bb228556b34
Zaidh <zadykhan29@gmail.com>
2024-12-25 14:10:02 +0000

    add Game class and requirements file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+class Game:
+    def __init__(self, title, genre, is_completed):
+        self.title = title
+        self.genre = genre
+        self.is_completed= is_completed
+
+        
